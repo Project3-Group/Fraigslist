e0d4dfebe0c985568fb19f0e77c7ad71b405ff4a
Zaya <zayazaw67@gmail.com>
2019-11-08 12:06:45 -0600

    basic create form, routes created

---
+++ controllers/items.js
@@ -0,0 +1,19 @@
+const db = require("../models");
+console.log("DB logs");
+console.log(db);
+// Defining methods for the ItemsController
+module.exports = {
+    findAll: function (req, res) {
+        db.Items
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        db.Items
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+};
