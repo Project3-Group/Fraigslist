796a0fe5aa8ab1cd7c5978f30ac03c5263ec4f47
Zaya <zayazaw67@gmail.com>
2019-11-09 14:23:45 -0600

    dynamic pages working. details loads few seconds after page loads though

--- client/src/pages/singleItem.js
+++ client/src/pages/singleItem.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react'
-import Navbar from '../components/navbar';
-import { Link } from 'react-router-dom';
-
-import Card from '../components/Card'
-import items from "../items.json";
 import API from '../utils/Api';
 
 
 class SingleItem extends Component {
     state = {
-        items: []
+        item: []
     };
 
     componentDidMount() {
         API.getItem(this.props.match.params.id)
             .then(res => this.setState({ item: res.data }))
-            // console.log(res.data)
             .catch(err => console.log(err));
     }
 
     render() {
         return (
             <div>test
-                {/* <div className="container">
+                <div className="container">
                     <div className="row">
-                        {this.state.item.name}
+                        {/* {console.log(this.state.item)} */}
+                        {this.state.item.itemName}
+                        {/* add more stuff that we want rendered about the page here. */}
                     </div>
-                </div> */}
+                </div>
             </div>
         )
 
