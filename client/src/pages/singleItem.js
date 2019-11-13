b5d09c83aa7e8fd39e6b0f5e712919feb96a421b
Orlando Corona <corona.orlando@gmail.com>
2019-11-13 13:56:01 -0600

    Oauth added

--- client/src/pages/singleItem.js
+++ client/src/pages/singleItem.js
@@ -42,34 +42,7 @@ class SingleItem extends Component {
         // console.log(this.props.match.params.id)
 
         /////////////////////////////////////////////////////////////
-        var transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                    type: "OAuth2",
-                    user: "24hrbidder@gmail.com",
-                    clientId: "866019043905-2gotkchkuachnjvvk9shbmhrrl3h5rkp.apps.googleusercontent.com",
-                    clientSecret: "XXJxWdO8hDPyjaIE728-aVVr",
-                    refreshToken: "1//04drX0PXBQmrDCgYIARAAGAQSNwF-L9IrtQJBpRF-8y2MGxbCWT_n-WNcxM4rDUQnwC3_FDb_6HfAMlaklr7LXQGD32-FGUqGNTk",
-                    accessToken: "ya29.Il-wB7ZN6d83Yo2OgjsCUBU49FQsSTcrmLCapQRBCckvy09ktcDhO1S1df5WdOyzbuMWZQP-LuZ9JioZicKLXMw29EjM-qbOiW2kchBcRnTcvBIJnzaPrZzpuylY0oFwEQ"
-               
-            }
-        })
         
-        var mailOptions = {
-            from: '"24hr Bidder" <24hrbidder@gmail.com>',
-            to: "corona.orlando@gmail.com",
-            subject: "Nodemailer test",
-            text: "Hello World"
-        }
-        
-        transporter.sendMail(mailOptions, function (err, res) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("Email Sent");
-        
-            }
-        })
         /////////////////////////////////////////////////////////////
 
         if (this.state.item.quantity - numPurchased.quantity >= 0) {
